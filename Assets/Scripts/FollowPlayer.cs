bbea9abf57fdb08c15f81fbce4204799cb528721
RileyH16 <ryjake16@icloud.com>
2024-01-22 12:18:50 -0600

    camera stuff

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-
-  
+    private FollowPlayer followPlayer;
+    public Transform cam;
     public Transform player;
+    public float maxpZ;
+    public float maxcZ;
+    private bool isFollow = true;
+    
 
 
+    public void Start()
+    {
+        isFollow = followPlayer;
+    }
 
-
-    void Update()
+    void FixedUpdate()
     {
 
         if (player != null)
         {
+            maxpZ = Mathf.Max(maxpZ, player.transform.position.z);
+            maxcZ = Mathf.Max(maxcZ, cam.transform.position.z);
+
+
             transform.position = player.transform.position + new Vector3(0, 11f, -7.5f);
             transform.rotation = Quaternion.Euler(new Vector3(50, 0, 0));
+
+            if (player.transform.position.z < maxpZ)
+            {
+                isFollow = false;
+                Debug.Log("should work");
+            }
+
+            
         }
 
     }
